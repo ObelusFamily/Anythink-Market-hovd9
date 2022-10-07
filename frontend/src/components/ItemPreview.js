d497203f30ab519426d1f19110edc496712a57e7
Michael Fitzpatrick <fitz@michaelfitzpatrick.co.uk>
2022-10-07 23:22:02 +0100

    Update ItemPreview.js

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import { IMAGE_PLACEHOLDER } from "../../constants/constants";
+import { IMAGE_PLACEHOLDER } from "../constants/constants";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
